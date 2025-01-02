9e9cf9c03c032c7351c06d4c0983ac8a78a86974
Maciej Lichoń <maciek.lichon@gmail.com>
2025-01-02 01:54:21 +0100

    add confirmation state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { CongratsComponent } from './components/congrats/congrats.component';
+import { TicketComponent } from './components/ticket/ticket.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FormComponent],
+  imports: [
+    HeaderComponent,
+    FormComponent,
+    LogoComponent,
+    CongratsComponent,
+    TicketComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
